bbe0351b3c9561c18c46858db211fe6291379582
adowmandek <adowmandek@gmail.com>
2021-02-21 12:11:02 +0300

    first commit

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,6 +1,11 @@
 fun main() {
-    val name:String="mandek"
-    val age:Int= 19
-    print(name)
-    print(age)
+    val name:String ="mandek adow"
+    val age=20
+    val phoneNumber="0794764520"
+    var kenyanCitizen:Boolean=true
+    val weight=43
+    println(name)
+    println(age)
+    println(phoneNumber)
+    println(kenyanCitizen)
 }
